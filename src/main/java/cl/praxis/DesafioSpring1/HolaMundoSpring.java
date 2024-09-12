8fc5ae89665f290e54ecd7652fa9fdc10afbc656
LucianoMoncada <lucianomoncada@gmail.com>
2024-09-11 21:34:17 -0300

    Desafio #1 usando Spring Boot, tema 6

---
+++ src/main/java/cl/praxis/DesafioSpring1/HolaMundoSpring.java
@@ -0,0 +1,13 @@
+package cl.praxis.DesafioSpring1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolaMundoSpring {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolaMundoSpring.class, args);
+	}
+
+}
